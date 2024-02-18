3c01636843b14e4fe43e27f54e3a4d627a1bff70
Federico Volpi <fedevolpi1@gmail.com>
2024-02-18 20:13:41 +0100

    - nuove logiche testate di dem e gro - r2 fixed

--- ParTree/classes/PrincipalParTree.py
+++ ParTree/classes/PrincipalParTree.py
@@ -10,6 +10,11 @@ from ParTree.algorithms.data_splitter import ObliqueHouseHolderSplit
 from ParTree.classes.ParTree import ParTree
 from ParTree.light_famd import MCA, PCA, FAMD
 
+from matplotlib import pyplot as plt
+from sklearn import tree
+
+from sklearn.base import clone
+
 
 class PrincipalParTree(ParTree):
     def __init__(
@@ -29,7 +34,8 @@ class PrincipalParTree(ParTree):
             verbose=False,
             mv=None,
             protected_attribute = None,
-            def_type = None
+            def_type = None,
+            filename = None
     ):
         """
         :param n_components:
@@ -61,6 +67,7 @@ class PrincipalParTree(ParTree):
         self.mv = mv
         self.protected_attribute = protected_attribute
         self.def_type = def_type
+        self.filename = filename
 
     def _calculate_similarity_matrix(self, points, threshold=9):
         points_arr = np.array(points)
@@ -71,9 +78,9 @@ class PrincipalParTree(ParTree):
         return adjacency_matrix
 
     def _write_to_file(self, content):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-        filename = f"output_{timestamp}.txt"
-        with open(filename, "a") as file:
+        #with open(filename, "a") as file:
+        #    file.write(content + "\n")
+        with open(self.filename, "a") as file:
             file.write(content + "\n")
 
     def _compute_penalty(self, points, labels):
@@ -81,6 +88,7 @@ class PrincipalParTree(ParTree):
         # here they choose the individual fairness
         n = len(points)
         penalties = np.zeros(n)
+        count_comp = 0
         # want to retrieve the row index of each point belonging to every cluster
         unique_labels = np.unique(labels)
         cluster_indices = {label: np.where(labels == label)[0] for label in unique_labels}
@@ -98,38 +106,54 @@ class PrincipalParTree(ParTree):
 
         # here users select the demographic parity definition
         elif self.def_type == 'dem':
+            print("----- START NEW SPLIT -----")
+            print("labels", labels)
             penalties = 0
             positive_prediction_rates = {}
             # we take each value of the protected attribute
             for cluster in set(labels):
+                print("CLUSTER", cluster)
                 X_filtered = self.X[cluster_indices[cluster]]
                 for group in np.unique(X_filtered[:, self.protected_attribute]):
+                    #print("group", group)
                     total_in_group = (X_filtered[:, self.protected_attribute]).shape[0]
                     positive_predictions_in_group = (
                                 X_filtered[:, self.protected_attribute] == group).sum()
                     positive_prediction_rate = positive_predictions_in_group / total_in_group
                     positive_prediction_rates[group] = positive_prediction_rate
+                keys = list(positive_prediction_rates.keys())
+                print("keys", keys)
+                for i, key1 in enumerate(keys):
+                    if positive_prediction_rates[key1] == 1:
+                        difference = 1
+                        penalties += difference
+                        count_comp += 1
+                    for key2 in keys[i + 1:]:
+                        if positive_prediction_rates[key1] == 1 and (key2 not in positive_prediction_rates or positive_prediction_rates[key2] == 1):
+                            difference = 1
+                        else:
+                            difference = abs(positive_prediction_rates[key1] - positive_prediction_rates[key2])
+                        penalties += difference
+                        count_comp += 1
 
-                for group1 in positive_prediction_rates:
-                    for group2 in positive_prediction_rates:
-                        if group1 != group2:
-                        # computation of the penalty as absolute difference between the prediction rates
-                            difference = abs(positive_prediction_rates[group1] - positive_prediction_rates[group2])
-                            penalties += difference
-                            print("penalties", penalties)
+            print("return ", penalties/count_comp)
+            return np.sum(penalties) / count_comp
 
-            return np.sum(penalties/len(labels))
         # third definition, group fairness in respect to proportion in the original dataset
         elif self.def_type == 'gro':
+            print("----- START NEW SPLIT -----")
 
             group_cluster_counts = {}
             group_counts = {}
+            total_cluster = {}
             penalties = 0
 
             # compute for each value of the protected attribute the number of points in each cluster
             for group in np.unique(self.X[:, self.protected_attribute]):
+                print("unique values protected attribute", np.unique(self.X[:, self.protected_attribute]))
                 group_counts[group] = (self.X[:, self.protected_attribute] == group).sum()
                 for label in np.unique(labels):
+                    total_cluster[label] = (labels == label).sum()
                     group_cluster_counts[(group, label)] = ((self.X[:, self.protected_attribute] == group) & (labels == label)).sum()
 
             total_count = self.X.shape[0]
@@ -138,14 +162,16 @@ class PrincipalParTree(ParTree):
             for group in np.unique(self.X[:, self.protected_attribute]):
                 total_in_group = group_counts[group]
                 if total_in_group > 0:  # Prevent division by zero
-                    tot_probability = total_in_group / total_count  # Proportion of group in the entire dataset
+                    tot_probability = total_in_group / total_count
+                    # Proportion of group in the entire dataset
                     for label in np.unique(labels):
                         group_cluster_count = group_cluster_counts[(group, label)]
-                        group_probability = group_cluster_count / total_in_group  # Proportion of group in the cluster
+                        group_probability = group_cluster_count / total_cluster[label]  # Proportion of group in the cluster
                         diff = abs(tot_probability - group_probability)  # Compute difference in probabilities
                         penalties += diff
+                        count_comp += 1
 
-            return np.sum(penalties) / len(labels)
+            return np.sum(penalties) / count_comp
 
         else:
             penalty = 0
@@ -162,7 +188,7 @@ class PrincipalParTree(ParTree):
 
         if len(self.con_indexes) == 0:  # all categorical
             transf = MCA(n_components=n_components_split, random_state=self.random_state)
-        elif len(self.cat_indexes) == 0:  # all continous
+        elif len(self.cat_indexes) == 0:  # all continou
             transf = PCA(n_components=n_components_split, random_state=self.random_state)
         else:  # mixed
             transf = FAMD(n_components=n_components_split, random_state=self.random_state)
@@ -178,62 +204,101 @@ class PrincipalParTree(ParTree):
 
         clf_list = list()
         labels_list = list()
-        # bic_c_list = list()
+        bic_c_list = list()
         r2_c_list = list()
 
         for i in trange(n_components_split, disable=not self.verbose):
-            clf_i = DecisionTreeRegressor(
-                max_depth=1,
-                min_samples_leaf=self.min_samples_leaf,
-                min_samples_split=self.min_samples_split,
-                random_state=self.random_state,
-            )
-
-            output_info = ""
-
             for feature_index in range(self.X.shape[1]):
-                best_split_score = float('inf')
-                best_split_value = None
-                best_r2_score = None
+                print("******************feature**************+****", feature_index)
+                clf_i = DecisionTreeRegressor(
+                    max_depth=3,
+                    min_samples_leaf=self.min_samples_leaf,
+                    min_samples_split=self.min_samples_split,
+                    random_state=self.random_state,
+                )
 
-                for value in np.unique(self.X[:, feature_index]):
+                best_split_score = -float('inf')
+                best_split_value = None
+                best_bic_score = None
+                best_r2_score = None
+                best_clf = None
+                best_labels = None
+
+                thresholds = sorted(np.unique(self.X[:, feature_index]))
+
+                for idx_threshold in range(len(thresholds)-1):
+                    value = thresholds[idx_threshold]
+                    value_succ = thresholds[idx_threshold+1]
+
                     modified_X = np.zeros(self.X.shape)
                     modified_X[self.X[:, feature_index] <= value, feature_index] = value
-                    modified_X[self.X[:, feature_index] > value, feature_index] = 2*(abs(value)+1)
+                    modified_X[self.X[:, feature_index] > value, feature_index] = value_succ
 
                     # Train the Decision Tree on the modified feature
                     clf_i.fit(modified_X[idx_iter], transf.fit_transform(self.X[idx_iter]))
                     labels_i = clf_i.apply(modified_X[idx_iter])
+                    #print("TO CHECK WITH THOSE labels_i", labels_i)
                     temp_score = clf_i.score(self.X[idx_iter], transf.fit_transform(self.X[idx_iter])) #da provare
+                    bic_children_i = bic(self.X[idx_iter], (np.array(labels_i) - 1).tolist())
+
                     if self.def_type is not None:
-                        penalty = self._compute_penalty(self.X[idx_iter], clf_i.apply(modified_X[idx_iter]))
+                        penalty = self._compute_penalty(self.X[idx_iter], labels_i)
                     else:
                         penalty = 0
-                    composite_score = temp_score - (abs(temp_score)*penalty)
+                    composite_score = temp_score - penalty
 
                     # Update the best split if this is better
-                    if composite_score < best_split_score:
+                    if composite_score > best_split_score:
                         best_split_score = composite_score
                         best_split_value = value
+                        best_bic_score = bic_children_i
                         best_r2_score = temp_score
+                        best_clf = clf_i
+                        best_labels = labels_i
 
                     # salvare valore split come value, penalty r2
 
-                    split_info = f"Feature {feature_index}, Value {value}, Best Split Score {best_split_score}, Penalty {penalty}, R2 Score {best_r2_score}\n"
-                    output_info += split_info
+                    self._write_to_file(f"\tFeature {feature_index}, Value {value}, Best Split Score {best_split_score}, Penalty {penalty}, R2 Score {best_r2_score}, BIC {bic_children_i}")
 
-                self._write_to_file(f"Best split for feature {feature_index}: Value {best_split_value}, Best Split Score {best_split_score}, R2 Score {best_r2_score}, Penalty {penalty}")
+                # clusters
+                if self.def_type == 'dem' or self.def_type == 'gro':
+                    # Arrays to hold indexes
+                    indexes_of_1 = []
+                    indexes_of_2 = []
 
-                if self.def_type == 'dem':
-                    #r2_children_i += abs(best_split_score) * penalty
-                    r2_children_i = abs(best_split_score) * penalty
+                    # Iterate through the list and append indexes accordingly
+                    for index, value in enumerate(labels_i):
+                        if value == 1:
+                            indexes_of_1.append(index)
+                        elif value == 2:
+                            indexes_of_2.append(index)
+
+                    protected_attribute_arr = self.X[:, self.protected_attribute]
+                    cluster_1 = [int(protected_attribute_arr[index]) for index in indexes_of_1]
+                    cluster_2 = [int(protected_attribute_arr[index]) for index in indexes_of_2]
+                    similar_count = None
+                elif self.def_type == 'ind':
+                    points = self.X[idx_iter]
+                    labels = clf_i.apply(modified_X[idx_iter])
+                    n = len(points)
+                    similarity_matrix = self._calculate_similarity_matrix(points)
+                    for i in range(n):
+                        cluster_indices = np.where(labels == labels[i])[0]
+                    similar_count = np.sum(similarity_matrix[i, cluster_indices]) - similarity_matrix[i, i]
+                    cluster_1 = None
+                    cluster_2 = None
                 else:
-                    #r2_children_i += abs(best_split_score)
-                    r2_children_i = abs(best_split_score)
+                    similar_count = None
+                    cluster_1 = None
+                    cluster_2 = None
 
-                r2_c_list.append(r2_children_i)
-                clf_list.append(clf_i)
-                labels_list.append(labels_i)
+                #print("LABELS LOG TO CHECK", labels_i)
+                self._write_to_file(f"\nBest split for feature {feature_index}: Value {best_split_value}, Best Split Score {best_split_score}, R2 Score {best_r2_score}, Penalty {penalty}, BIC {best_bic_score}, \nLabels {labels_i}, Cluster 1 {cluster_1}, Cluster 2 {cluster_2}, [IND] similar count {similar_count}\n")
+
+                r2_c_list.append(best_split_score)
+                clf_list.append(best_clf)
+                labels_list.append(best_labels)
+                bic_c_list.append(best_bic_score)
 
             if self.oblique_splits and i > 0:
                 olq_clf_i = ObliqueHouseHolderSplit(
@@ -246,16 +311,19 @@ class PrincipalParTree(ParTree):
 
                 olq_clf_i.fit(self.X[idx_iter], y_pca[:, i])
                 olq_labels_i = olq_clf_i.apply(self.X[idx_iter])
-                olq_r2_children_i = r2_score(y_pca[:, i], olq_predictions_i)
+                olq_r2_children_i = r2_score(self.X[idx_iter], (np.array(labels_i) - 1).tolist())
 
                 clf_list.append(olq_clf_i)
                 labels_list.append(olq_labels_i)
                 r2_c_list.append(olq_r2_children_i)
 
-        idx_min = np.argmin(r2_c_list)
+        idx_min = np.argmax(r2_c_list)
         is_oblique = self.oblique_splits and idx_min > 0 and idx_min % 2 == 0
         labels = labels_list[idx_min]
+        bic_children = bic_c_list[idx_min]
         r2_children = r2_c_list[idx_min]
         clf = clf_list[idx_min]
-        self._write_to_file(f"Overall Best Split: Feature Index {idx_min}, R2 Children {r2_children}, Is Oblique {is_oblique}")
-        return clf, labels, r2_children, is_oblique
+        self._write_to_file(f"Overall Best Split: Feature Index {idx_min}, Bic Children {bic_children}, Is Oblique {is_oblique}, R2 Children {r2_children}\n")
+
+        #print("labels", labels)
+        return clf, labels, bic_children, is_oblique
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import time
 from datetime import datetime
+from fairlearn.metrics import demographic_parity_difference
 
+import os
 import numpy as np
 import pandas as pd
 from sklearn.compose import make_column_selector, ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import LabelEncoder
 
 from ParTree.algorithms.measures_utils import get_metrics_uns, get_metrics_s
 from ParTree.classes.CenterParTree import CenterParTree
@@ -13,13 +17,19 @@ from ParTree.classes.ParTree import print_rules
 from ParTree.classes.PrincipalParTree import PrincipalParTree
 from ParTree.classes.VarianceParTree import VarianceParTree
 
+timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+filename = os.path.join("logs", f"output_{timestamp}.txt")
+
 if __name__ == '__main__':
     data = pd.read_json('Experiments/datasets/real/genfair_toy.json')
     #data = pd.read_csv('Experiments/datasets/real/compas-scores-two-years_y.zip')
     #data = pd.read_csv('Experiments/datasets/syntetic/2d-4c_y.zip')
-    print(data)
+    #data = pd.read_csv('Experiments/datasets/real/german_credit_y.zip')
+    #data = pd.read_csv('Experiments/datasets/real/bank.zip')
 
     print(data.columns)
+    data = data.head(500)
+    #data = data.iloc[:, :10]
 
     cptree = CenterParTree(
         max_depth=3,
@@ -35,15 +45,40 @@ if __name__ == '__main__':
         n_jobs=6,
         verbose=True
     )
+    #index = data.columns.tolist().index('sex')
     #cptree = ImpurityParTree(n_jobs=1, max_nbr_values_cat=np.inf)
-    cptree = PrincipalParTree(2, 2, 3, 5, np.inf, np.inf, 0.0, 1, 1, False, 0, mv=None, def_type="dem", protected_attribute=3)
+    cptree = PrincipalParTree(5, 2, 3, 5, np.inf, np.inf, 0.0, 1, 1, False, 0, mv=None, def_type="gro", protected_attribute=0, filename=filename)
     #cptree = VarianceParTree(2, 2, 3, 5, 100, 100, 0.0, 42, 1, False)
+
+#    class BinaryEncoder(TransformerMixin):
+#        def __init__(self):
+#            self.encoder = LabelEncoder()
+
+#        def fit(self, X, y=None):
+#            self.encoder.fit(X)
+#            return self
+
+#        def transform(self, X, y=None):
+#            return self.encoder.transform(X)
+
+ #       def get_params(self, deep=True):
+ #           return {}
+
+#        def set_params(self, **params):
+#            return self
+
+
+#    def select_transformer(column):
+#        if len(column.unique()) == 2:
+#            return (f'binary_{column.name}', BinaryEncoder(), [column.name])
+#        else:
+#            return (f'onehot_{column.name}', OneHotEncoder(), [column.name])
 
     def cluster_info(obj):
         n_cluster = len(np.unique(obj.labels_))
         bic = "%.4f" % obj.bic_
         #r2 = "%.4f" % obj.r2_
-        return r2, n_cluster
+        return bic, n_cluster
 
     if cptree.def_type != "ind" and cptree.def_type != None:
         protected_attribute_index = cptree.protected_attribute
@@ -55,28 +90,30 @@ if __name__ == '__main__':
     #print(data.dtypes)
     ct = ColumnTransformer([
     #('std_scaler', scaler, make_column_selector(dtype_include=['int', 'float'])),
-    #("cat", OrdinalEncoder(), make_column_selector(dtype_include="object")),
+    ("cat", OrdinalEncoder(), make_column_selector(dtype_include="object", pattern=f'^(?!{protected_attribute_index}$).*$')),
     #("cat", OneHotEncoder(), make_column_selector(dtype_include="object"))
-    ("cat", OneHotEncoder(), make_column_selector(dtype_include="object", pattern=f'^(?!{protected_attribute_index}$).*$'))
+    #("cat", OneHotEncoder(), make_column_selector(dtype_include="object", pattern=f'^(?!{protected_attribute_index}$).*$'))
         ],
         remainder='passthrough', verbose_feature_names_out=False, sparse_threshold=0, n_jobs=12)
     #ct = ColumnTransformer(transformers, remainder='passthrough', verbose_feature_names_out=False, sparse_threshold=0,
     #                       n_jobs=12)
 
     transformed_data = pd.DataFrame(ct.fit_transform(data), columns=ct.get_feature_names_out())
+    print("COLUMN NAMES: ", ct.get_feature_names_out())
 
     if cptree.def_type == "dem" or cptree.def_type == "gro":
 
         # Identify columns in transformed_data that are related to the protected attribute
         protected_cols = [col for col in transformed_data.columns if col.startswith(protected_attribute_name)]
+        #print("protected cols", protected_cols)
 
         # All columns in transformed_data that are not related to the protected attribute
         other_cols = [col for col in transformed_data.columns if col not in protected_cols]
 
         # Reconstruct the column order to place the protected columns at the original index
         new_order = other_cols[:protected_attribute_index] + protected_cols + other_cols[protected_attribute_index:]
+        #print("new order", new_order)
         data = transformed_data[new_order]
-
     else:
         data = transformed_data
 
@@ -88,10 +125,18 @@ if __name__ == '__main__':
     #    ],
     ##    remainder='passthrough', verbose_feature_names_out=False, sparse_threshold=0, n_jobs=12)
 
+    #data = pd.DataFrame(ct.fit_transform(data), columns=ct.get_feature_names_out())
+    #data = pd.concat([protected_attr_series.reset_index(drop=True), data.reset_index(drop=True)], axis=1)
+    #data.insert(protected_attribute_index, 'protected attribute', protected_attr_series)
+    #data = pd.DataFrame(ct.fit_transform(data))
+
+    #data = data.drop(data.columns[1], axis=1)
+    print("PRE COL", data.columns)
     X = data.values[:, :-1]
     y = data.values[:, -1]
 
     data = X
+    print(data)
     labels = y
     n_real_cluster = len(np.unique(y))
 
@@ -110,8 +155,6 @@ if __name__ == '__main__':
 
     formatted_rules = print_rules(cptree.get_rules(), X.shape[1])
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-    filename = f"output_{timestamp}.txt"
-    print("labels cptree", cptree.labels_)
+    #print("labels cptree", cptree.labels_)
     with open(filename, "a") as file:
         file.write(formatted_rules + "\n")
