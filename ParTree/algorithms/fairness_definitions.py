d15b39106322adcdba63327588afa919fd6d4789
cri98li <cri98li@gmail.com>
2024-03-26 18:25:36 +0100

    Refactoring, optimization and suggestions

--- ParTree/algorithms/fairness_definitions.py
+++ ParTree/algorithms/fairness_definitions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def _calculate_similarity_matrix(points):
+def _calculate_adjacency_matrix(points):
     points_arr = np.array(points)
     diff = points_arr[:, np.newaxis, :] - points_arr[np.newaxis, :, :]
     dist_matrix = np.linalg.norm(diff, axis=-1)
@@ -11,12 +11,14 @@ def _calculate_similarity_matrix(points):
 
 
 def _fairness_ind(n, points, labels):
+    # Provare con kdtree (magari mean_distance si può stimare, o potremmo modificare la definizione di
+    # individual fairness guardando alle proprietà di kdtree
     count_comp = 0
     penalties = 0
-    similarity_matrix = _calculate_similarity_matrix(points)
+    adjacency_matrix = _calculate_adjacency_matrix(points)
     for i in range(n):
         cluster_indices = np.where(labels == labels[i])[0]
-        similar_count = np.sum(similarity_matrix[i, cluster_indices]) - similarity_matrix[i, i]
+        similar_count = np.sum(adjacency_matrix[i, cluster_indices]) - adjacency_matrix[i, i]
         penalty = similar_count / len(cluster_indices)
         count_comp += 1
         penalties += penalty
@@ -33,10 +35,9 @@ def _fairness_dem(points, labels, cluster_indices, protected_attribute):
     # we take each value of the protected attribute
     for cluster in set(labels):
         X_filtered = points[cluster_indices[cluster]]
+        total_in_group = X_filtered.shape[0]
         for group in np.unique(points[:, protected_attribute]):
-            total_in_group = (X_filtered[:, protected_attribute]).shape[0]
-            positive_predictions_in_group = (
-                    X_filtered[:, protected_attribute] == group).sum()
+            positive_predictions_in_group = (X_filtered[:, protected_attribute] == group).sum()
             positive_prediction_rate = positive_predictions_in_group / total_in_group
             positive_prediction_rates[group] = positive_prediction_rate
 
@@ -44,12 +45,11 @@ def _fairness_dem(points, labels, cluster_indices, protected_attribute):
 
         for i, key1 in enumerate(keys):
             for key2 in keys[i + 1:]:
-                # else:
                 difference = abs(positive_prediction_rates[key1] - positive_prediction_rates[key2])
                 penalties += difference
                 count_comp += 1
 
-    penalty = np.sum(penalties) / count_comp
+    penalty = penalties / count_comp
     return penalty
 
 
@@ -70,13 +70,13 @@ def _fairness_gro(points, labels, protected_attribute):
 
     total_count = points.shape[0]
 
-    for group in np.unique(points[:, protected_attribute]):
-        total_in_group = group_counts[group]
+    for group, total_in_group in group_counts.items():
+        # CRI: la divisione per zero quando può capitare?
         if total_in_group > 0:  # Prevent division by zero
             tot_probability = total_in_group / total_count
-            for label in np.unique(labels):
+            for label, value in total_cluster.items():
                 group_cluster_count = group_cluster_counts[(group, label)]
-                group_probability = group_cluster_count / total_cluster[label]
+                group_probability = group_cluster_count / value
                 diff = abs(tot_probability - group_probability)
                 penalties += diff
                 count_comp += 1
